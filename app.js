fcad45deb0b90bbe74062b322e5e812bc0fc8917
ayakakojimaak <125942459+ayakakojimaak@users.noreply.github.com>
2024-11-29 11:51:57 -0800

    add modal

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
 $(function () {
-  const previousBtn = $("header>button:nth-child(1)");
-  const nextBtn = $("header>button:nth-child(2)");
-
   const setContents = (user, post, todo) => {
     // user
     const infoContent = `
@@ -18,30 +15,66 @@ $(function () {
     if (post.length) {
       let postContent = "";
       post.forEach((data) => {
-        postContent = `
+        postContent += `
           <li class="post-${data.id}">
             <h4 class="js-modal-open" data-id="${data.id}">${data.title}</h4>
             <p>${data.body}</p>
           </li>`;
       });
       $(".posts > ul").html(postContent);
-      // modal
     } else {
-      $(".posts > ul").html("User has no posts");
+      $(".posts > ul").html("<li>User has no posts</li>");
     }
     // todo
     if (todo.length) {
       let todoContent = "";
       todo.forEach((data) => {
-        todoContent = `
+        todoContent += `
           <li class="todo-${data.id}">
             <p>${data.todo}</p>
           </li>`;
       });
       $(".todos > ul").html(todoContent);
     } else {
-      $(".todos > ul").html("User has no todos");
+      $(".todos > ul").html("<li>User has no todos</li>");
     }
+    // modal
+    const modalContent = `
+      <div class="overlay" style="display:none;">
+        <div class="modal">
+          <h1></h1>
+          <p></p>
+          <div></div>
+          <button class="js-modal-close">Close Modal</button>
+        </div>
+      </div>`;
+    $("body").append(modalContent);
+    $(".js-modal-open").on("click", function () {
+      const postId = $(this).data("id");
+      openModal(postId);
+    });
+    $(".js-modal-close").on("click", function () {
+      $(".overlay").fadeOut();
+    });
+  };
+
+  const openModal = (postId) => {
+    $.ajax({
+      url: `https://dummyjson.com/posts/${postId}`,
+      dataType: "json",
+    })
+      .done(function (data) {
+        const data_stringify = JSON.stringify(data);
+        const data_json = JSON.parse(data_stringify);
+        console.log(data_json);
+        $(".modal h1").html(data.title);
+        $(".modal p").html(data.body);
+        $(".modal div").html(`View: ${data.views}`);
+        $(".overlay").fadeIn();
+      })
+      .fail(function () {
+        alert("error");
+      });
   };
 
   const getData = (userId) => {
@@ -60,11 +93,14 @@ $(function () {
       type: "GET",
       dataType: "json",
     };
-    $.when($.ajax(userData), $.ajax(postData), $.ajax(todoData)).done(function (user, post, todo) {
-      setContents(user[0], post[0].posts, todo[0].todos);
-    });
+    $.when($.ajax(userData), $.ajax(postData), $.ajax(todoData))
+      .done(function (user, post, todo) {
+        setContents(user[0], post[0].posts, todo[0].todos);
+      })
+      .fail(function () {
+        alert("error");
+      });
   };
-  // Posts by post id (for the modal): [https://dummyjson.com/posts/${postid}]
 
   const getParams = () => {
     const urlParams = new URLSearchParams(window.location.search);
@@ -88,6 +124,9 @@ $(function () {
 
   // add event listener
   // page transition
+  const previousBtn = $("header>button:nth-child(1)");
+  const nextBtn = $("header>button:nth-child(2)");
+
   previousBtn.on("click", function () {
     const userId = getParams();
     const newId = userId === 1 ? 30 : userId - 1;
